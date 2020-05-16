0c44fdf213448b2ad1d9332f831434117fc5780d
biletskadi <61203571+biletskadi@users.noreply.github.com>
2020-05-16 17:52:47 +0300

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,7 +1,7 @@
 import mapbox
 import json
 
-geocoder = mapbox.Geocoder(access_token='sk.eyJ1IjoiZGlhbmthbG9sNCIsImEiOiJjazhzdjE4c3QwMnlwM2Rud2EwZzg1b29iIn0.OqtyNqmiJI5q6UbWQC6oCQ')
+geocoder = mapbox.Geocoder(access_token='...')
 response = geocoder.forward('Paris, France')
 with open('text.json', 'w', encoding='UTF-8') as f:
     json.dump(response.json(), f)
